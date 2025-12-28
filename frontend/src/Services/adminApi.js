efdce1d5d53910ef22861ad550aea8fae2ee73e5
Jaydipsinh000 <jaydipsinhg555@gmail.com>
2025-12-28 17:55:32 +0530

    sever link error

--- frontend/src/Services/adminApi.js
+++ frontend/src/Services/adminApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://carent-dgkk.onrender.com/api" || "http://localhost:5000/api",
+  baseURL: "https://carent-07up.onrender.com/api" || "http://localhost:5000/api",
 });
 
 /* TOKEN */
--- frontend/src/Services/authServices.js
+++ frontend/src/Services/authServices.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://carent-dgkk.onrender.com/api/user" || "http://localhost:5000/api/user";
+const API_URL = "https://carent-07up.onrender.com/api/user" || "http://localhost:5000/api/user";
 
 export const registerUser = (userData) => {
   return axios.post(`${API_URL}/register`, userData, {
